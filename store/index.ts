49a73b65fc351cbba9a20172c016350ee9cbe921
Robin Joseph <robinjo1776@gmail.com>
2026-01-11 08:20:33 +0530

    updated code

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
 import projectReducer from "./slices/projectSlice";
@@ -12,18 +11,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-=======
-import { configureStore } from "@reduxjs/toolkit";
-import themeReducer from "./slices/themeSlice";
-import projectReducer from "./slices/projectSlice";
 
-export const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-    projects: projectReducer,
-  },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
->>>>>>> e7b500a4787e9b495d567a5254f9d942c3897de8
