09fb17ef40e68d1c7abe051ae65c918343fc30f0
Robin Joseph <robinjo1776@gmail.com>
2026-01-29 22:51:34 +0530

    note added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
     <div className="p-8">
       <h2 className="text-3xl font-bold mb-2">My Work</h2>
 
-      <p className="text-sm text-gray-600 mb-6">
-        <span className="font-semibold text-blue-600">Note:</span> Backend
+      <p className="text-md mb-6">
+        <span className="font-semibold text-blue-600">Note:</span>Backend
         services are temporarily disabled in all projects to reduce hosting
         costs. Full end-to-end demos can be made available upon request.
       </p>
